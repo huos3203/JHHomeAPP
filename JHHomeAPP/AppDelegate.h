7d3a8be29e2e26a4f9ca6e860bc6ca2cb06617ce
huos3203 <724987481@qq.com>
2018-06-23 07:20:25 +0800

    init

---
+++ JHHomeAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JHHomeAPP
+//
+//  Created by admin on 2018/6/23.
+//  Copyright © 2018年 huoshuguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JHHomeAPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JHHomeAPP
+//
+//  Created by admin on 2018/6/23.
+//  Copyright © 2018年 huoshuguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
